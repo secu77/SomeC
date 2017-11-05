44e6047bc78d6911f81ad7f63ed6cfc231727b85
Raul <security@stationx11.es>
2017-11-05 20:39:47 +0100

    Update Copiar.c
    Mejorando la forma de lectura y escritura, mediante bloques de 1024 bytes.

--- copiar.c
+++ copiar.c
@@ -1,16 +1,17 @@
 /*
- *  Copia.c Version 1.0
+ *  Copiar.c Version 1.1
  *
  *  Este programa te permite hacer un duplicado de un archivo pasado por argumentos a otro destino, 
  *  también pasado por argumento. 
- *  El programa determina los bytes del archivo a copiar, reserva un buffer de esa cantidad, y posteriormente
- *  lee dicho archivo, lo traslada al buffer, y del buffer lo escribe al archivo destino. Creando un proceso hijo,
- *  que será el que lea de la tuberia y escriba en el archivo final.
+ *  El programa determina los bytes del archivo a copiar, hace una lectura continua en bloques de 1024 bytes,
+ *  y los va copiando en la tuberia, de igual forma el padre va leyendo en bloques de 1024 bytes
+ *  y escribiendo dicho bloque de bytes en el archivo final.
  *
- *  Este programa no satisface el objetivo real, ya que la transmisión debería hacerse por bloques de 'x' bytes,
- *  ya que así te evitas reservar un buffer muy grande de memoria.
+ *  El procedimiento es: El padre crea un hijo que lee el archivo bytes a bytes (en bloques de 'LENBLOCK'),
+ *  los pasa a la tuberia con la que se comunica con el padre, y este finalmente escribe dichos bloques en
+ *  el archivo resultante. Va mostrando (de forma muy simple) como va cada bloque.
  *
- *  Sería funcional para archivos pequeños que copiar, pero no para archivos de gran tamaño.
+ *  Se ha solucionado el problema de reservar un bloque de memoria demasiado grande.
  *
  *  A espera de revisiones y mejoras.
  *
@@ -20,9 +21,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#define LENBLOCK 1024   // Tamaño del Bloque de Bytes.
 
 /*
  *  Función que obtiene el tamaño en bytes de un archivo pasandole la ruta de este.
@@ -49,15 +53,13 @@ int main(int argc, char **argv){
   char *bufferout;
   char *path;
   char *outpath;
-  long numbytes = 0;
-  buffer = (char *)calloc(numbytes,sizeof(char));
-  bufferout = (char *)calloc(numbytes,sizeof(char));
-  int descriptor, outcriptor, reader, writer, pid, estado;
+  long numbytes = 0, readpipe = 0;
+  int descriptor, outcriptor, reader, pid;
   int fd[2];
 
 
   /*
-   *  Validando argumentos introducidos en el inicio
+   *  Validando argumentos introducidos en el inicio.
    */	
   if(argc == 3){
     path = argv[1];
@@ -66,7 +68,7 @@ int main(int argc, char **argv){
     pipe(fd);
 
     /*
-     *  Validando la obtención del número de bytes de la ruta del archivo
+     *  Validando la obtención del número de bytes de la ruta del archivo.
      */
      numbytes = getBytes(path);
      if(numbytes < 1){
@@ -76,12 +78,7 @@ int main(int argc, char **argv){
 
 
     /*
-     *  Creacion de Procesos:
-     *
-     *   -El proceso Padre lee el fichero que se le pasa por argumentos, determina el numero de
-     *    de bytes a extraer y los escribe en la tuberia.
-     *   -El proceso Hijo comprueba si existe el fichero pasado por segundo argumento y si puede
-     *    crearlo, lee de la tuberia los bytes pasados por el padre y los escribe en el archivo.
+     *  Creacion de Procesos. (Explicado en el header)
      */
     pid = fork();
     switch(pid){
@@ -95,85 +92,57 @@ int main(int argc, char **argv){
        *  Proceso Hijo
        */
       case 0:
-        
-        /*
-	 *  Cerrando el descriptor de la tuberia en modo escritura y leyendo los
-	 *  bytes del modo lectura
-	 */      
-        close(fd[1]);
-        read(fd[0],bufferout,numbytes);
+      
+        close(fd[0]);                                      // Cerrando el modo lectura de la tuberia.
+        buffer = (char *)malloc(LENBLOCK/sizeof(char));    // Reservo 1024 bytes es decir, espacio para 1024 chars.
 
-        //printf("\nSoy el hijo y he leido de la tuberia %ld bytes\n",numbytes);
-
-        /*
-	 *  Validando la apertura/creacion del archivo de destino
-	 */
-	outcriptor = open(outpath, O_WRONLY|O_CREAT|O_TRUNC, 0622);
-	if(outcriptor < 0){
-	  printf("\nError al crear el archivo de destino %s\n",outpath);
-	  exit(-1);
-	}
-
-        /*
-         *  Validando la escritura de bytes sobre el archivo destino
-         */
-        writer = write(outcriptor,bufferout,numbytes);
-        if(writer < 0){
-      	  printf("\nError al escribir en el archivo %s\n",outpath);
-  	  exit(-1);
-        }else{
-      	  //printf("\n[OK]Todo se escribió genial en el archivo %s\n",outpath);
+        // Validando la apertura del archivo pasado por argumentos
+        descriptor = open(path, O_RDONLY);
+        if(descriptor < 0){
+          printf("\nError al abrir el archivo %s\n",path);
+          exit(-1);
         }
 
-        /*
-         *  Liberando memoria, cerrando descriptores y finalizando el hijo.
-         */
-        free(bufferout);
-        close(outcriptor);
+        printf("\nCopiando: [");
+        // Leyendo del fichero 1 y escribiendo los bloques en la tuberia.
+        while((reader = read(descriptor,buffer,LENBLOCK)) != 0){
+          //printf("\nBuffer:{%s}\n", buffer);
+          write(fd[1],buffer,reader);
+          printf("-");
+        }
+
+        printf(">]\t (*)Archivo copiado con éxito!\n\n");
+
+        close(descriptor);
         exit(0);
+
         break;
 
       /*
        *  Proceso Padre
        */
       default:
-        close(fd[0]);
 
-            /*
-	     *  Validando la apertura del archivo pasado por argumentos
-	     */
-	    descriptor = open(path, O_RDONLY);
-	    if(descriptor < 0){
-	      printf("\nError al abrir el archivo %s\n",path);
-	      exit(-1);
-	    }
+      close(fd[1]);                                        // Cerrando el modo escritura en la tuberia.
+      wait(NULL);                                          // Esperando a que el hijo finalice su escritura en la tuberia.
+      bufferout = (char*)malloc(LENBLOCK/sizeof(char));    // Reservando un bloque de 1024 bytes como antes para el buffer.
 
-	    /*
-	     *  Validando la lectura de bytes del archivo origen
-	     */
-	    reader = read(descriptor,buffer,numbytes);
-	    if(reader < 0){
-	      printf("\nError al leer el archivo %s\n",path);
-	      exit(-1);
-	    }else{
-	      //printf("\n[*]Numero de bytes leidos: %ld\n",numbytes);
-	    }
-        
-        /*
-	 *  Escribiendo los bytes del archivo a la tuberia y esperando a que
-	 *  el hijo acabe con una salida satisfactoria
-	 */        
-        write(fd[1], buffer, numbytes);
-        //printf("\nEscritos %ld en la tuberia por el padre\n",numbytes);
-        wait(&estado);
-        //printf("\nMurio el hijo con un exit %d\n", ((estado>>8) & 0xFF));
+      // Lectura por bloques hasta el último.
+      while((readpipe = read(fd[0],bufferout,LENBLOCK)) != 0){
 
-        /*
-         *  Liberando memoria y cerrando descriptores
-         */
-        close(descriptor);
-  	free(buffer);
-		    
+        // Validando la apertura/creacion del archivo de destino.
+        outcriptor = open(outpath, O_WRONLY|O_CREAT|O_APPEND, 0666);
+        if(outcriptor < 0){
+          printf("\nError al crear el archivo de destino %s\n",outpath);
+          exit(-1);
+        }
+
+        // Escribiendo en el archivo el bloque y poniendo a cero la memoria del buffer.
+        write(outcriptor,bufferout,readpipe);
+        memset(bufferout,0,LENBLOCK);
+      }
+
+      close(outcriptor);  
     }
 
   }else{
